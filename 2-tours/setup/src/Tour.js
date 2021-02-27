2f3cc4ed90e9d7504795ddcece22f11930a6fc2b
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-02-27 00:25:09 -0600

    toggle button, fetch data

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, price, name, removeTour}) => {
+  const [readMore, setReadMore] = useState(false)
+  //readmore 
+  return <article className='single-tour'>
+    <img src={image} alt={name} />
+    <footer>
+      <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>$ {price}</h4>
+      </div>
+      <p>
+        {/* substring makes so that you only show a certain number of characters.  */}
+        {readMore ? info : `${info.substring(0, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>{readMore ? 'show less' : 'show more'}?</button>
+      </p>
+      <button className='delete-btn' onClick={() => removeTour(id)}>Clear</button>
+    </footer>
+  </article>
 };
 
 export default Tour;
--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+  console.log(tours)
+  return (
+    <section>
+      <div className='title'>
+        <h2>Our Tours </h2>
+        <div className='underline'></div>
+        <div>
+          {tours.map((tour) => {
+            return (
+              // you can use the destructure of tour here to make it simple to pass into the props 
+              <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
