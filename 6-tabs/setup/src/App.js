da2bf63eae5f6ad3f5ff6649d3ff0ee1097bd790
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-03-03 17:38:45 -0600

    changing css for active element

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -35,7 +35,7 @@ function App() {
     <div className='job-center'>
       <div className='btn-containter'>
         {jobs.map((item, index) => {
-          return <button key={item.id} onClick={() => setValue(index)}>
+          return <button key={item.id} onClick={() => setValue(index)} className={`job-btn ${index === value && 'active-btn'}`}>
             {item.company}
           </button>
         })}
