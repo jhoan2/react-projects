345643b97a8265524b12bd480abce2ce022bd7f0
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-03-03 15:43:15 -0600

    manually filtering by category

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+//issue with manual filtering by category is that we are not in sync with our data. 
+//What this means that is that when add more categories you will have to manually add each of them. 
+const Categories = ({filterItems}) => {
+  return <div className='btn-container'>
+      <button className='filter-btn' onClick={() => filterItems('all')}>all</button>
+      <button className='filter-btn' onClick={() => filterItems('breakfast')}>breakfast</button>
+    </div>;
 };
 
 export default Categories;
