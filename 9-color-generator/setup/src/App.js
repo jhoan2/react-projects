30b541ae595d407c42c052b4304c888ebcf6e362
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-05-13 12:13:34 -0700

    init

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,32 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState();
+  //For times when the color is wrong 
+  const [error, setError] = useState(false);
+  //For the default value in the submit 
+  const [list, setList] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+  return (
+  <>
+    <section className='container'>
+      <h3>Color Generator</h3> 
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#4169E1' />
+        <button className='btn' type='submit'>
+          Submit
+        </button>
+      </form>
+    </section>
+    <section className='colors'>
+      <h4>List goes here </h4> 
+    </section>
+  </>
+  )
 }
 
 export default App
