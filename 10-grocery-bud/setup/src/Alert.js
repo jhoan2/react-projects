6a023e1e70d00ca1c96cab3c3e35e779ead4756e
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-05-14 12:17:26 -0700

    remove item w/o redux

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({msg, type, removeAlert}) => {
+const Alert = ({msg, type, removeAlert, list}) => {
+  //adding list to dependency array so that it waits 3 seconds before disappearing 
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert()
     }, 3000)
     return () => clearTimeout(timeout)
-  },[])
+  },[list])
 
   return (
   <p className={`alert alert-${type}`}>{msg}</p>
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -35,10 +35,15 @@ function App() {
     showAlert(true, 'danger', 'cleared list')
     setList([])
   }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item) => item.id !== id))
+  }
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert = {showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert = {showAlert} list={list} />}
         <h3> Grocery Bud </h3>
         <div className='form-control'>
           <input type='text' className='grocery' placeholder='e.g eggs' value={name} onChange={(e) => setName(e.target.value)} />
@@ -49,7 +54,7 @@ function App() {
       </form>
       {list.length > 0 && (
       <div className='grocery-container'>
-        <List items={list} />
+        <List items={list} removeItem={removeItem} />
         <button className='clear-btn' onClick={clearList} > Clear Items </button>
       </div>
       )}
