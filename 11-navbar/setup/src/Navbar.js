89853f99b7122564be1bd9ee102f4c46177b603c
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-03 19:31:07 -0700

    map to make links and social icons

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -13,24 +13,20 @@ const Navbar = () => {
         </button>
       </div>
       <div className="links-container show-container">
-        
+        <ul className='links'>
+          {links.map((link) => { const {id,url,text} = link;
+        return <li key={id}>
+          <a href={url}>{text}</a>
+        </li>})}
+        </ul>
       </div>
       <ul className='social-icons'>
-        <li>
-          <a href='https://www.twitter.com'>
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com'>
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com'>
-            <FaTwitter />
-          </a>
-        </li>
+        {social.map((socialIcon) => {
+          const {id,url,icon} = socialIcon;
+          return <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        })}
       </ul>
     </div>
   </nav>
