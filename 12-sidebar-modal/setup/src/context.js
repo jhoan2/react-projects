dda65431476e5575d8492980c68a825c2c627acf
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-04 13:54:05 -0700

    useContext to wrap app and ueGlobalContext

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,16 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+//wrapping the entire app in this 
+//always make sure to display the children or you won't see anything 
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+}
+
+// custom hook 
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
